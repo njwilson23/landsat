cc05d845d7fc0f518bf58e469b4196d90d1e0de0
Nat Wilson <njwilson23@gmail.com>
2015-07-25 23:24:53 -0700

    constructor pipelines for QueryParameters
    also, Request now takes a QueryParameters pointer

--- query.go
+++ query.go
@@ -4,6 +4,8 @@ import (
 	"encoding/xml"
 	"fmt"
 	"github.com/njwilson23/geometry"
+	"strings"
+	//"html/template"
 	"io/ioutil"
 	"net/http"
 	"time"
@@ -20,9 +22,49 @@ const (
 )
 
 type QueryParameters struct {
-	Bbox   [4]float64
-	Dates  [2]time.Time
-	Sensor string // "LANDSAT_8"
+	Bbox    [4]float64
+	Dates   [2]time.Time
+	Sensor  string // "LANDSAT_8"
+	WRSPath int32
+	WRSRow  int32
+}
+
+func (q *QueryParameters) ByBbox(lon0 float64, lon1 float64, lat0 float64, lat1 float64) *QueryParameters {
+	q.Bbox = [4]float64{lon0, lon1, lat0, lat1}
+	return q
+}
+
+func (q *QueryParameters) ByDateRange(t0 time.Time, t1 time.Time) *QueryParameters {
+	q.Dates = [2]time.Time{t0, t1}
+	return q
+}
+
+func (q *QueryParameters) BySensor(sens string) *QueryParameters {
+	q.Sensor = sens
+	return q
+}
+
+func (q *QueryParameters) ByWRSPath(path int32) *QueryParameters {
+	q.WRSPath = path
+	return q
+}
+
+func (q *QueryParameters) ByWRSRow(row int32) *QueryParameters {
+	q.WRSRow = row
+	return q
+}
+
+func NewQuery() *QueryParameters {
+	q := QueryParameters{
+		[4]float64{-180, 180, -90, 90},
+		[2]time.Time{
+			time.Date(1900, time.January, 1, 0, 0, 0, 0, time.UTC),
+			time.Date(3000, time.January, 1, 0, 0, 0, 0, time.UTC),
+		},
+		LandsatAll,
+		-1,
+		-1}
+	return &q
 }
 
 type LandsatScene struct {
@@ -71,15 +113,26 @@ type XMLResponse struct {
 
 // Send a request to the Landsat Bulk Metadata server, and return the XML
 // response as bytes
-func Request(q QueryParameters) ([]byte, error) {
+func Request(q *QueryParameters) ([]byte, error) {
 	var result []byte
 
 	const datefmt = "2006-01-02"
+	//request := "http://earthexplorer.usgs.gov/EE/InventoryStream/"
 	request := fmt.Sprintf(
 		`http://earthexplorer.usgs.gov/EE/InventoryStream/latlong?north=%v&south=%v&east=%v&west=%v&sensor=%v&start_date=%v&end_date=%v`,
 		q.Bbox[3], q.Bbox[2], q.Bbox[1], q.Bbox[0],
 		q.Sensor,
 		q.Dates[0].Format(datefmt), q.Dates[1].Format(datefmt))
+
+	if q.WRSPath != -1 {
+		pathstr := fmt.Sprintf(`&wrspath=%v`, q.WRSPath)
+		request = strings.Join([]string{request, pathstr}, "")
+	}
+
+	if q.WRSRow != -1 {
+		rowstr := fmt.Sprintf(`&wrsrow=%v`, q.WRSRow)
+		request = strings.Join([]string{request, rowstr}, "")
+	}
 
 	resp, err := http.Get(request)
 	if err != nil {
@@ -95,12 +148,23 @@ func Request(q QueryParameters) ([]byte, error) {
 	return result, err
 }
 
-// Convert an XML response into a list of Scene structs
-func ParseXMLBytes(r []byte) ([]LandsatScene, error) {
+// Convert an XML byte data into a list of Scene structs
+func ParseXMLBytes(data []byte) ([]LandsatScene, error) {
 	response := XMLResponse{}
-	err := xml.Unmarshal(r, &response)
+	err := xml.Unmarshal(data, &response)
 	if err != nil {
 		fmt.Println(err)
 	}
 	return response.Scenes, err
 }
+
+// Display a list of Landsat scenes in an HTML table
+/*func DisplayWeb(ls []LandsatScene) (string, error) {
+	//t, err := template.New("lsgrid").ParseFiles("templates/lsgrid.html")
+	t, err := template.New("lsgrid").Parse("<div class=\"scene\" style=\"background-image: {{BrowseUrl}}\"></div>")
+	if err != nil {
+		fmt.Println(err)
+	}
+	t.ExecuteTemplate(out, "lsgrid", []LandsatScene)
+	return "", err
+}*/
--- query_test.go
+++ query_test.go
@@ -7,33 +7,44 @@ import (
 )
 
 func TestRequest(t *testing.T) {
-	q := QueryParameters{[4]float64{-71, -70, 41, 42},
-		[2]time.Time{time.Date(2015, time.February, 25, 0, 0, 0, 0, time.UTC),
-			time.Date(2015, time.March, 7, 0, 0, 0, 0, time.UTC)},
-		Landsat8}
+
+	q := NewQuery()
+	q.ByDateRange(
+		time.Date(2015, time.February, 25, 0, 0, 0, 0, time.UTC),
+		time.Date(2015, time.March, 7, 0, 0, 0, 0, time.UTC))
+	q.ByBbox(-71, -70, 41, 42)
+	q.BySensor(Landsat8)
+
 	req, err := Request(q)
+
 	if err != nil {
 		fmt.Println(err)
+		t.Fail()
 	}
+
 	if len(req) == 0 {
 		t.Fail()
 	}
+
 }
 
 func TestParseRequest(t *testing.T) {
-	q := QueryParameters{[4]float64{-72, -69, 41, 42},
-		[2]time.Time{
-			time.Date(2015, time.February, 23, 0, 0, 0, 0, time.UTC),
-			time.Date(2015, time.March, 7, 0, 0, 0, 0, time.UTC),
-		},
-		Landsat8}
 
-	req, err := Request(q)
+	q := NewQuery()
+	q.ByDateRange(
+		time.Date(2015, time.February, 23, 0, 0, 0, 0, time.UTC),
+		time.Date(2015, time.March, 7, 0, 0, 0, 0, time.UTC))
+	q.ByBbox(-72, -69, 41, 42)
+	q.BySensor(Landsat8)
+
+	result, err := Request(q)
 	if err != nil {
 		fmt.Println(err)
+		t.Fail()
 	}
 
-	scenes, _ := ParseXMLBytes(req)
+	scenes, _ := ParseXMLBytes(result)
+	fmt.Println(len(scenes))
 	for i, s := range scenes {
 		fmt.Println(i, s)
 	}
